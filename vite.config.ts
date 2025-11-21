251ae0b1f5bfcc0402c71be84fc9fe7083a83de9
vishalkmar <vk722413@gmail.com>
2025-11-21 18:18:21 +0530

    second commit before deploy

--- vite.config.ts
+++ vite.config.ts
@@ -28,7 +28,8 @@ export default defineConfig({
   },
   root: path.resolve(import.meta.dirname, "client"),
   build: {
-    outDir: path.resolve(import.meta.dirname, "dist/public"),
+    // YAHAN CHANGE:
+    outDir: path.resolve(import.meta.dirname, "dist"),
     emptyOutDir: true,
   },
   server: {
